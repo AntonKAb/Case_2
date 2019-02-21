66f17a5ddb43a27689b3ab2182fb8182992c13d2
dodiko1d <kirill.anufrienko@gmail.com>
2019-02-21 20:27:58 +0700

    Data reading updated.

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ def file_reading():
         data = petrols_file.readlines()
         for petrol in data:
             petrol_data = petrol.rstrip().split()
-            petrol_stations.update({petrol_data[0]: {'cars_number': petrol_data[1], 'oil': []}})
+            petrol_stations.update({int(petrol_data[0])-1: {'cars_number': int(petrol_data[1]), 'oil': []}})
             for oil in petrol_data[2:]:
-                petrol_stations[petrol_data[0]]['oil'].append(oil)
+                petrol_stations[int(petrol_data[0])-1]['oil'].append(oil)
     with open('input.txt') as clients_file:
         data = clients_file.readlines()
         number = 0
         for client in data:
             client_data = client.rstrip().split()
-            clients.update({number: {'arrival': client_data[0], 'time': client_data[1], 'oil': client_data[2]}})
+            clients.update({number: {'arrival': client_data[0], 'time': int(client_data[1]), 'oil': client_data[2]}})
             number += 1
     return {'petrol_stations': petrol_stations, 'clients': clients}
+
+
+print(file_reading()['clients'])
